5b45f85de282507b612d4a45ffdabee90b84bed6
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-10-31 20:11:16 +0000

    add initial nvim config

--- configurations/programs/nvim/default.nix
+++ configurations/programs/nvim/default.nix
@@ -3,5 +3,9 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./set.lua}
+    '';
   };
 }
---
+++ configurations/programs/nvim/set.lua
@@ -0,0 +1,3 @@
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.scrolloff = 20
