35645a0d18dc60aabd35b4c56708e5080cc68484
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-05 03:09:41 +0000

    add lua_ls to nixos config

--- home/users/work/programs/nvim/default.nix
+++ home/users/work/programs/nvim/default.nix
@@ -4,6 +4,10 @@
     enable = true;
     defaultEditor = true;
 
+    extraPackages = with pkgs; [
+	lua-language-server
+    ];
+
     extraLuaConfig = ''
       ${builtins.readFile ./init.lua}
     '';
