02aceceef13f7f4db42173db915940cd74d6aa43
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-05 02:54:46 +0000

    add mini.pick to nvim

--- home/users/work/programs/nvim/init.lua
+++ home/users/work/programs/nvim/init.lua
@@ -1,3 +1,10 @@
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-vim.opt.scrolloff = 20
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.shiftwidth = 4
+vim.o.scrolloff = 20
+
+vim.pack.add({
+    { src = "https://github.com/echasnovski/mini.pick" }
+})
+
+require "mini.pick".setup()
