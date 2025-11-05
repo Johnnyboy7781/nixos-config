636a6265987397b6aad00b3b05da45e4e5779708
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-05 03:17:12 +0000

    add tab nvim settings

--- home/users/work/programs/nvim/init.lua
+++ home/users/work/programs/nvim/init.lua
@@ -1,12 +1,16 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
 vim.o.scrolloff = 20
+vim.o.signcolumn = "yes"
 vim.g.mapleader = " "
 
 vim.pack.add({
-    { src = "https://github.com/echasnovski/mini.pick" },
-    { src = "https://github.com/neovim/nvim-lspconfig" }
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" }
 })
 
 require "mini.pick".setup()
@@ -15,4 +19,3 @@ vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 
 vim.lsp.enable({ "lua_ls" })
-
