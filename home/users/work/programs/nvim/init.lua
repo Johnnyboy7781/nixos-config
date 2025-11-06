8c2b3fdbd219330778ab2de5c6f9826ff1ec6ba9
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-06 02:36:59 +0000

    add write keymap for nvim

--- home/users/work/programs/nvim/init.lua
+++ home/users/work/programs/nvim/init.lua
@@ -30,5 +30,6 @@ vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover)
 vim.keymap.set('n', '<leader>e', ':Oil --float .<CR>')
+vim.keymap.set('n', '<leader>w', ':write')
 
 vim.lsp.enable({ "lua_ls", "nil_ls" })
