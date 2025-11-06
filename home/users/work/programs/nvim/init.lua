46b3d8e341e4aa53b4b3fd24302a8d7e21c56514
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-06 05:42:18 +0000

    add peek commands

--- home/users/work/programs/nvim/init.lua
+++ home/users/work/programs/nvim/init.lua
@@ -61,3 +61,6 @@ vim.lsp.enable({
     "angularls",
     "java_language_server",
 })
+
+vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
