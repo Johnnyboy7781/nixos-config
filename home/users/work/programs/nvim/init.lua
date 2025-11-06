179a3d352f055b9259b92c353ef7b620d16c3660
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-06 18:28:35 +0000

    fix treesitter url

--- home/users/work/programs/nvim/init.lua
+++ home/users/work/programs/nvim/init.lua
@@ -22,7 +22,7 @@ vim.pack.add({
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/nvim-mini/mini.comment" },
     {
-        src = "https://github.com/nvim-treesitter/nvim-treesitter/tree/main",
+        src = "https://github.com/nvim-treesitter/nvim-treesitter",
         data = {
             lazy = false,
             branch = "main",
