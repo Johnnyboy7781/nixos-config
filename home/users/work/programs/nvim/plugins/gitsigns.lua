dac7095b8454e346ef67345d45382aee50e78b4c
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-11 21:06:42 +0000

    add keymaps for gitsigns

--- home/users/work/programs/nvim/plugins/gitsigns.lua
+++ home/users/work/programs/nvim/plugins/gitsigns.lua
@@ -1,1 +1,36 @@
-require "gitsigns".setup()
+require "gitsigns".setup({
+    on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- nav
+        map('n', ']g', function()
+            if vim.wo.diff then
+                vim.cmd.normal({']g', bang = true})
+            else
+                gitsigns.nav_hunk('next')
+            end
+        end)
+
+        map('n', '[g', function()
+            if vim.wo.diff then
+                vim.cmd.normal({'[g', bang = true})
+            else
+                gitsigns.nav_hunk('prev')
+            end
+        end)
+
+        -- action
+        map('n', '<leader>gr', gitsigns.reset_hunk)
+        map('n', '<leader>gR', gitsigns.reset_buffer)
+        map('n', '<leader>gp', gitsigns.preview_hunk_inline)
+        map('n', '<leader>gp', function()
+            gitsigns.blame_line({full = true})
+        end)
+    end
+})
