869d423343cb41dd663e7d79368a7695cf88f28d
Jonathan McDonnell <jonathan.mcdonnell@kinsaleins.com>
2025-11-24 13:44:53 -0500

    add paste to clipboard provider

--- home/users/work/programs/nvim/clipboard.lua
+++ home/users/work/programs/nvim/clipboard.lua
@@ -4,5 +4,9 @@ vim.g.clipboard = {
         ['+'] = 'clip.exe',
         ['*'] = 'clip.exe'
     },
+    paste = {
+        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
     cache_enabled = 1
 }
